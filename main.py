b46af06e2b543041a4ff2d942237b13d5e4cb39a
BernardoTxr <bernardoteixeira@usp.br>
2025-05-31 13:38:21 -0300

    .

--- main.py
+++ main.py
@@ -1,27 +1,31 @@
 from fastapi import FastAPI, UploadFile, File
 from typing import List
 from fastapi.responses import JSONResponse
-#from docling.document_converter import DocumentConverter
-import pdfplumber 
+
+import pdfplumber
 from fastapi.middleware.cors import CORSMiddleware
 import os
 
 app = FastAPI()
 
+
 @app.get("/health")
 def health():
     return "OK", 200
 
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
+
 @app.post("/processar")
 async def processar_pdf(files: List[UploadFile] = File(...)) -> JSONResponse:
+    textos = []
     for file in files:
         filename = file.filename.lower()
 
@@ -34,27 +38,25 @@ async def processar_pdf(files: List[UploadFile] = File(...)) -> JSONResponse:
         with open(input_path, "wb") as f:
             f.write(contents)
 
-        #converter = DocumentConverter()
-        #result = converter.convert(input_path)
-        #with open(input_path.replace(".pdf", ".txt"), "w") as f:
-            #f.write(result.document.export_to_markdown())
-        
         with pdfplumber.open(input_path) as pdf:
             texto = ""
             for page in pdf.pages:
-                texto += (page.extract_text() or "") + "\n"  # Garante que não quebre se a página não tiver texto
+                texto += (
+                    page.extract_text() or ""
+                ) + "\n"  # Garante que não quebre se a página não tiver texto
 
-        with open(input_path.replace(".pdf", ".txt"), "w", encoding="utf-8") as f:
-            f.write(texto)
+        textos.append(texto)
 
-    #Possibilidade de chamar outra api
+    # Possibilidade de chamar outra api
     return JSONResponse(
         content={
-            "message": texto,
-            "status": "success"
+            "message": textos,
+            "filenames": [file.filename for file in files],
+            "status": "success",
         },
-        status_code=200
+        status_code=200,
     )
+
 
 import os
 
@@ -62,5 +64,5 @@ port = int(os.environ.get("PORT", 8000))
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
 
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
